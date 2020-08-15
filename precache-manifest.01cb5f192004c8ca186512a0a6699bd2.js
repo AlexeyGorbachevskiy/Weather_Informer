5be78f1aff853d6fabe05c251eef09eebd0caee5
AlexeyGorbachevskiy <alexeygorbachevskiyy@gmail.com>
2020-08-15 22:08:11 +0300

    Updates

---
+++ precache-manifest.01cb5f192004c8ca186512a0a6699bd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf943508ed1f40a516c191cd4106445",
+    "url": "/Weather_Informer/index.html"
+  },
+  {
+    "revision": "5e87e469e0f7ad587a06",
+    "url": "/Weather_Informer/static/css/main.4817ef9c.chunk.css"
+  },
+  {
+    "revision": "f373a5c02388721967b4",
+    "url": "/Weather_Informer/static/js/2.fc7c5eaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather_Informer/static/js/2.fc7c5eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e87e469e0f7ad587a06",
+    "url": "/Weather_Informer/static/js/main.6e8f4963.chunk.js"
+  },
+  {
+    "revision": "e388a321145bb60a1a89",
+    "url": "/Weather_Informer/static/js/runtime-main.68b44894.js"
+  },
+  {
+    "revision": "9b44a343acbd2a61619857e5002ceda4",
+    "url": "/Weather_Informer/static/media/wave.9b44a343.svg"
+  }
+]);
